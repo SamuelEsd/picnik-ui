7f0414097668fb4bfbc372d2a7bf5b4b93eeec55
Samuel Perea <mr.samuel.perea@gmail.com>
2025-10-15 22:23:19 -0600

    Add exctactor from picnik

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import picnik as pnk
+from picnik import DataExtraction as DE
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 
 
@@ -10,6 +12,14 @@ st.title("CSV File Upload")
 
 # Create file uploader widget that accepts multiple files
 uploaded_files = st.file_uploader("Choose CSV files (minimum 2, maximum 20)", type=['csv'], accept_multiple_files=True)
+
+file_paths = []
+# Get the full path to the files (if running locally and files are saved temporarily)
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        with open(os.path.join("/tmp", uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        file_paths.append(os.path.join("/tmp", uploaded_file.name))
 
 # Check number of uploaded files
 num_files = len(uploaded_files)
@@ -51,6 +61,7 @@ if num_files > 0:
             for i, valid_file in enumerate(valid_files, 1):
                 st.success("iterating: " + str(i))
                 df = valid_file
+                st.write(f"**File {i}:** {uploaded_files[i-1]}")
                 filename = uploaded_files[i-1].name
                 files_names_list.append(filename)
 
@@ -60,8 +71,27 @@ if num_files > 0:
                     st.write(f"**Columns:** {df.columns.tolist()}")
                     st.dataframe(df)
             st.button("Reset", type="primary")
-            if st.button("Extrcat Data"):
+            if st.button("Extract Data"):
+                st.write("Uploaded file names:")
+                st.write(files_names_list)
+
+                st.write("file paths:")
+                st.write(file_paths)
+
                 st.write("Extracting Data!")
+                # Extract data from uploaded files
+                data_extractor = DE()
+                Bnum, T0num = data_extractor.read_files(file_paths)
+                st.write("Files to be used: \n{}\n ".format(file_paths))
+
+                st.write(f'The computed heating rates are:\n')
+                for b in range(len(data_extractor.Beta)):
+                    st.write(f'\n{data_extractor.Beta[b]:6.3f} +/- {data_extractor.BetaError[b]:.3f} K/min\n')
+
+
+                data_extractor.plot_data(x_data='temperature', x_units='K')
+                data_extractor.Conversion([data_extractor.DFlis[k]['Temperature [K]'].values[0] for k in range(len(Bnum))],[data_extractor.DFlis[k]['Temperature [K]'].values[-1] for k in range(len(Bnum))])  # calculation of the conversion degree in the temperature range (Ti,Tf)
+                isoTables_num = data_extractor.Isoconversion(d_a=0.02)
             else:
                 st.write("")
 else:
